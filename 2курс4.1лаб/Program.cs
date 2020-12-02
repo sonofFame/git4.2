f09a6758ff14a1b8207514b2ef71109097a223b6
egorka.kamenev17@gmail.com <egorka.kamenev17@gmail.com>
2020-12-02 18:18:05 +0700

    Добавьте файлы проекта.

---
+++ 2курс4.1лаб/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2курс4._1лаб
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
